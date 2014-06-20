4d4f6f980f90516efcb1643fd68f6de517e5fcfd
sah0017 <shammond@faulkner.edu>
2014-06-20 11:27:47 -0500

    Initial commit

---
+++ 6700/primeFactors/testPrimeFactor.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 19, 2014
+
+@author: susanha
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testprimeFactors(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testprimeFactors']
+    unittest.main()
