b73ad3c617066cd817b008e05e93d132f9431226
sah0017 <shammond@faulkner.edu>
2014-06-20 14:01:34 -0500

    third green light

--- 6700/primeFactors/Factor.py
+++ 6700/primeFactors/Factor.py
@@ -9,6 +9,10 @@ Created on Jun 20, 2014
 def primeFactorsOf(myInteger):
     myFactors = []
     if myInteger > 1:
+        if (myInteger % 2) == 0:
+            myFactors.append(2)
+            myInteger = myInteger / 2
+    if myInteger > 1:
         myFactors.append(myInteger)
     return myFactors
 
--- 6700/primeFactors/testPrimeFactor.py
+++ 6700/primeFactors/testPrimeFactor.py
@@ -13,6 +13,8 @@ class Test(unittest.TestCase):
     def testprimeFactors(self):
         self.assertEqual(Factor.primeFactorsOf(1),[])
         self.assertEqual(Factor.primeFactorsOf(2), [2])
+        self.assertEqual(Factor.primeFactorsOf(3), [3])
+        self.assertEqual(Factor.primeFactorsOf(4), [2,2])
         
 
 
